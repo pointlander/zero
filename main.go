9f338677c0efdb498bb4db740a1b15eef92915b0
Andrew Snodgrass <git@pointlander.info>
2023-03-14 20:15:54 -0500

    Added complex autoencoder mode

--- main.go
+++ main.go
@@ -1566,4 +1566,93 @@ func main() {
 		}
 		return
 	}
+
+	rnd := rand.New(rand.NewSource(1))
+
+	set := tc128.NewSet()
+	set.Add("A", Width, Width)
+	for _, w := range set.Weights {
+		factor, size := math.Sqrt(2.0/float64(w.S[0])), cap(w.X)
+		for i := 0; i < size; i++ {
+			w.X = append(w.X, complex(factor*rnd.NormFloat64(), factor*rnd.NormFloat64()))
+		}
+		w.States = make([][]complex128, 1)
+		for i := range w.States {
+			w.States[i] = make([]complex128, len(w.X))
+		}
+	}
+
+	other := tc128.NewSet()
+	other.Add("X", Width, Length/2)
+	x := other.ByName["X"]
+	for i := 0; i < Length/2; i++ {
+		if i < Length/4 {
+			x.X = append(x.X, cmplx.Rect(vectors[i], math.Pi/4))
+		} else {
+			x.X = append(x.X, complex(vectors[i], 0))
+		}
+	}
+
+	l1 := tc128.Mul(set.Get("A"), other.Get("X"))
+	cost := tc128.Avg(tc128.Quadratic(other.Get("X"), l1))
+
+	iterations := 128
+	points := make(plotter.XYs, 0, iterations)
+	phase := make(plotter.XYs, 0, iterations)
+	alpha, eta := complex(.3, 0), complex(.3, 0)
+	for i := 0; i < iterations; i++ {
+		set.Zero()
+		other.Zero()
+
+		total := tc128.Gradient(cost).X[0]
+		norm := complex128(0)
+		for _, d := range x.D {
+			norm += d * d
+		}
+		norm = cmplx.Sqrt(norm)
+		scaling := complex(1, 0)
+		if cmplx.Abs(norm) > 1 {
+			scaling = 1 / norm
+		}
+		for _, w := range set.Weights {
+			for l, d := range w.D {
+				w.States[0][l] = alpha*w.States[0][l] - eta*d*scaling
+				w.X[l] += w.States[0][l]
+			}
+		}
+		if cmplx.Abs(total) < 1e-6 {
+			break
+		}
+
+		points = append(points, plotter.XY{X: float64(i), Y: float64(cmplx.Abs(total))})
+		phase = append(phase, plotter.XY{X: float64(i), Y: float64(cmplx.Phase(total))})
+		fmt.Println(i, cmplx.Abs(total))
+	}
+
+	p := plot.New()
+
+	p.Title.Text = "epochs vs cost"
+	p.X.Label.Text = "epochs"
+	p.Y.Label.Text = "cost"
+
+	scatter, err := plotter.NewScatter(points)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	scatter, err = plotter.NewScatter(phase)
+	if err != nil {
+		panic(err)
+	}
+	scatter.GlyphStyle.Radius = vg.Length(1)
+	scatter.GlyphStyle.Shape = draw.CircleGlyph{}
+	p.Add(scatter)
+
+	err = p.Save(8*vg.Inch, 8*vg.Inch, "complex_cost.png")
+	if err != nil {
+		panic(err)
+	}
 }
